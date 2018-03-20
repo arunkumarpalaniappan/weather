eb71463ac10577e84d9deca125f835e00b67ec79
Arunkumar Palaniappan <11667975+arunkumarpalaniappan@users.noreply.github.com>
2018-03-20 08:50:53 +0530

    Update googleMapsAPI.js

--- src/api/googleMapsAPI.js
+++ src/api/googleMapsAPI.js
@@ -2,7 +2,17 @@ import 'whatwg-fetch'
 
 class googleMapsApi {
     static getGooglePlaces(locString) {
-        return fetch('https://maps.googleapis.com/maps/api/place/autocomplete/json?input=' + locString + '&types=geocode&key=AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo')
+        return fetch('https://maps.googleapis.com/maps/api/place/autocomplete/json?input=' + locString + '&types=geocode&key=AIzaSyAyesbQMyKVVbBgKVi2g6VX7mop2z96jBo', {
+             method: 'GET',
+             type: 'cors',
+             headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin':'*',
+                'Access-Control-Allow-Credentials': true,
+                'Access-Control-Allow-Methods': 'GET'
+               }
+            })
             .then(function (response) {
                 return response.json()
             }).then(function (json) {
